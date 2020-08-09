eabef478fe1e0b41e79da8cc2a43d88fd6cebff4
yaronyes <yaronyes@hotmail.com>
2020-08-09 14:12:24 +0300

    adding todo data model + componenet

---
+++ src/components/Todos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Todos = () => {
+
+    return (
+        <div className="todos-comp">
+            <h1>Todos</h1>
+        </div>
+    );
+}
+
+export default Todos;
---
+++ src/model/TodosModel.js
@@ -0,0 +1,6 @@
+export default class TodoModel {
+    constructor(text, isCompleted) {
+        this.text = text;
+        this.isCompleted = isCompleted;
+    }
+}
