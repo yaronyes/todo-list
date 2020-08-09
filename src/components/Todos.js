576f0a17d625081196afe938d1347dc858279ff1
yaronyes <yaronyes@hotmail.com>
2020-08-09 14:21:59 +0300

    adding input and handle enter key

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Todos = () => {
+    const [value, setValue] = useState("");
+    
+    const addTodo = (event) => {                
+        if(event.keyCode === 13) {
+            alert(value)
+        }
+    }
 
     return (
         <div className="todos-comp">
             <h1>Todos</h1>
+            <input type="text" value={value} placeholder="What's next?" onChange={(e) => setValue(e.target.value) }  onKeyUp={addTodo}/>
         </div>
     );
 }
