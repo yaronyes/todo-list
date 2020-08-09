aa035830c34e11dc65ed946594c044dc0a18f10f
yaronyes <yaronyes@hotmail.com>
2020-08-09 22:14:23 +0300

    Updates

---
+++ precache-manifest.f0ddf03878a213845ff9e0267cd96efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f55e72869c433a9b4ea97a80bb471a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7bd4d1d0f87a904ea859",
+    "url": "/todo-list/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "975ecdb72bab8a87a548",
+    "url": "/todo-list/static/css/main.a15bb08c.chunk.css"
+  },
+  {
+    "revision": "7bd4d1d0f87a904ea859",
+    "url": "/todo-list/static/js/2.5a55dc65.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-list/static/js/2.5a55dc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975ecdb72bab8a87a548",
+    "url": "/todo-list/static/js/main.577efe10.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
